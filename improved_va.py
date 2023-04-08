221d6af6ee215657ae6056402ff9b9368cebfd1c
Daniel Abreu <psydp1@nottingham.ac.uk>
2023-04-08 04:23:17 +0100

    Added while loop

--- improved_va.py
+++ improved_va.py
@@ -3,16 +3,17 @@ from EdgeGPT import Chatbot, ConversationStyle
 import re #deals with unwanted text formatting
 
 async def main():
-    bot = Chatbot(cookiePath='cookies.json')
-    response = await bot.ask(prompt=input("Ask Bing AI a question..."), conversation_style=ConversationStyle.creative)
-    # Select only the bot response from the response dictionary
-    for message in response["item"]["messages"]:
-        if message["author"] == "bot":
-            bot_response = message["text"]
-    #Removes citations from responses
-    bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
-    print("Bot's response:", bot_response)
-    await bot.close()
+    while True:
+        bot = Chatbot(cookiePath='cookies.json')
+        response = await bot.ask(prompt=input("Ask Bing AI a question..."), conversation_style=ConversationStyle.creative)
+        # Select only the bot response from the response dictionary
+        for message in response["item"]["messages"]:
+            if message["author"] == "bot":
+                bot_response = message["text"]
+        #Removes citations from responses
+        bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
+        print("Bot's response:", bot_response)
+        await bot.close()
 
 if __name__ == '__main__':
     asyncio.run(main())
