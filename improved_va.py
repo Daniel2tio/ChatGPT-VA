62d7dc60a791950f3fb1cd9ec632d1847081d6c2
Daniel Abreu <psydp1@nottingham.ac.uk>
2023-04-08 22:24:21 +0100

    Base function of the VA is complete

--- improved_va.py
+++ improved_va.py
@@ -1,3 +1,4 @@
+import openai
 import asyncio
 import whisper 
 import speech_recognition as sr #for capturing audio
@@ -6,43 +7,52 @@ import re #deals with unwanted text formatting
 import boto3
 import pydub
 from pydub import playback
+
+openai.api_key = "OpenAI API Key if using it"
+
 #Function to recognize wake up word
-recognizer = sr.Recognizer() 
-wake_word = "bing"
+recognizer = sr.Recognizer()
+BING_WAKE_WORD = "bing"
+GPT_WAKE_WORD = "gpt"
 
 def get_wake_word(phrase):
-    if wake_word in phrase.lower():
-        return wake_word
+    if BING_WAKE_WORD in phrase.lower():
+        return BING_WAKE_WORD
+    elif GPT_WAKE_WORD in phrase.lower():
+        return GPT_WAKE_WORD
     else:
         return None
     
 #Function to output text into speech using Amazon's polly synthesizer
-def speech_synth(text, output_filename):
-    polly = boto3.client("polly", region_name="eu-west-2")
+def synthesize_speech(text, output_filename):
+    polly = boto3.client('polly', region_name='eu-west-2')
     response = polly.synthesize_speech(
         Text=text,
-        OutputFormat = 'mp3',
-        VoiceId = 'Joey',
-        Engine='neural' #most natural sounding voice
+        OutputFormat='mp3',
+        VoiceId='Joey',
+        Engine='neural'
     )
+    with open(output_filename, 'wb') as f:
+        f.write(response['AudioStream'].read())
 
 #Function that plays the synthesized audio
-def play(file):
-    sound = pydub.AudioSegment.from_file(file, format='mp3')
+def play_audio(file):
+    sound = pydub.AudioSegment.from_file(file, format="mp3")
     playback.play(sound)
 
 #Function that retrieves responses from Bing AI
 async def main():
     while True:
-        with sr.Microphone() as source: #accesses the device's microphone to record audio
+
+        with sr.Microphone() as source:
             recognizer.adjust_for_ambient_noise(source)
-            print(f"Waiting for wake words 'ok bing' or 'ok chat'...")
+            print(f"Waiting for wake word 'Bing'...")
             while True:
-                audio = recognizer.listen(source)#records the audio
+                audio = recognizer.listen(source)
                 try:
-                    with open ("audio.wav", "wb") as f:
+                    with open("audio.wav", "wb") as f:
                         f.write(audio.get_wav_data())
-                    #using the preloaded "tiny" model
+                    # Use the preloaded tiny_model
                     model = whisper.load_model("tiny")
                     result = model.transcribe("audio.wav")
                     phrase = result["text"]
@@ -54,30 +64,70 @@ async def main():
                     else:
                         print("Not a wake word. Try again.")
                 except Exception as e:
-                    print("Say that again please".format(e)) #handles an error using exception if the try block fails
+                    print("Error transcribing audio: {0}".format(e))
+                    continue
 
-            print("Tell me a prompt...")
-            speech_synth("What can I help you with?", "response.mp3")
-            play("response.mp3")
+            print("Speak a prompt...")
+            synthesize_speech('What can I help you with?', 'response.mp3')
+            play_audio('response.mp3')
             audio = recognizer.listen(source)
 
+            try:
+                with open("audio_prompt.wav", "wb") as f:
+                    f.write(audio.get_wav_data())
+                model = whisper.load_model("base")
+                result = model.transcribe("audio_prompt.wav")
+                user_input = result["text"]
+                print(f"You said: {user_input}")
+            except Exception as e:
+                print("Error transcribing audio: {0}".format(e))
+                continue
 
-        bot = Chatbot(cookiePath='cookies.json')
-        response = await bot.ask(prompt=input("Ask Bing AI a question..."), conversation_style=ConversationStyle.creative)
-        # Select only the bot response from the response dictionary
-        for message in response["item"]["messages"]:
-            if message["author"] == "bot":
-                bot_response = message["text"]
-        #Removes citations from responses
-        bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
+            if wake_word == BING_WAKE_WORD:
+                bot = Chatbot(cookiePath='cookies.json')
+                response = await bot.ask(prompt=user_input, conversation_style=ConversationStyle.precise)
+
+                for message in response["item"]["messages"]:
+                    if message["author"] == "bot":
+                        bot_response = message["text"]
+
+                bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
+
+                bot = Chatbot(cookiePath='cookies.json')
+                response = await bot.ask(prompt=user_input, conversation_style=ConversationStyle.creative)
+                # Select only the bot response from the response dictionary
+                for message in response["item"]["messages"]:
+                    if message["author"] == "bot":
+                        bot_response = message["text"]
+                # Remove [^#^] citations in response
+                bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
+
+            else:
+                # Send prompt to GPT-3.5-turbo API
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content":
+                        "You are a helpful assistant."},
+                        {"role": "user", "content": user_input},
+                    ],
+                    temperature=0.5,
+                    max_tokens=150,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                    n=1,
+                    stop=["\nUser:"],
+                )
+
+                bot_response = response["choices"][0]["message"]["content"]
+                
         print("Bot's response:", bot_response)
-        speech_synth(bot_response, 'response.mp3')
-        play('response.mp3')
+        synthesize_speech(bot_response, 'response.mp3')
+        play_audio('response.mp3')
         await bot.close()
 
-#Function that responds to an audio wake up word using whisper
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
 
     
