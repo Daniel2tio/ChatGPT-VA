f03bfbc96299542a70a3f6bf7d535dcd409f30d4
Daniel Abreu <psydp1@nottingham.ac.uk>
2023-04-08 04:22:24 +0100

    Created function that uses BingAI's reverse engineered api

--- improved_va.py
+++ improved_va.py
@@ -1,9 +1,17 @@
 import asyncio
 from EdgeGPT import Chatbot, ConversationStyle
+import re #deals with unwanted text formatting
 
 async def main():
     bot = Chatbot(cookiePath='cookies.json')
-    print(await bot.ask(prompt="Hello World", conversation_style=ConversationStyle.creative))
+    response = await bot.ask(prompt=input("Ask Bing AI a question..."), conversation_style=ConversationStyle.creative)
+    # Select only the bot response from the response dictionary
+    for message in response["item"]["messages"]:
+        if message["author"] == "bot":
+            bot_response = message["text"]
+    #Removes citations from responses
+    bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
+    print("Bot's response:", bot_response)
     await bot.close()
 
 if __name__ == '__main__':
