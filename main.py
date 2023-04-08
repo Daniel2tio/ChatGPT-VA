a9e924f86dbbff1425a34b2278edeb6abf4f42ae
Daniel Abreu <psydp1@nottingham.ac.uk>
2023-04-08 01:56:08 +0100

    Imported dependencies

---
+++ main.py
@@ -0,0 +1,8 @@
+import openai #allows the access to gpt api
+import pyttsx3 #allows the transcribing of text to speech
+import speech_recognition as sr #allows the transcribing of speech to text
+import time 
+
+#Setting the OpenAI API Key
+
+openai.api_key = ""
